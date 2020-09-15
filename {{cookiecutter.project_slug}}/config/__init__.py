2dfbd0383a13a20ea5ed0a1fe314c0a358ca07eb
gunjanmodi <er.gunjanmodi@gmail.com>
2020-09-15 19:29:08 +0530

    removing config directory

--- {{cookiecutter.project_slug}}/config/__init__.py
+++
@@ -1,6 +0,0 @@
-{% if cookiecutter.use_celery == 'y' -%}
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-
-__all__ = ("celery_app",)
-{% endif -%}
